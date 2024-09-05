45b86605c6d196c11da8c7e0d5b9685d8c9b9c05
AnnePassos <annecpassos@outlook.com>
2024-09-05 19:07:37 -0300

    Aula 1

---
+++ imc.js
@@ -0,0 +1,18 @@
+var name_1;
+var name_2;
+
+name_1="João";
+console.log(name_1);
+name_2="Maria";
+console.log(name_2);
+
+//calcular o imc de uma pessoa . IMC=PESO/altura)²
+
+var peso;
+var altura;
+
+peso=70;
+altura=1.70;
+imc = peso / altura ** 2 
+
+console.log("Seu imc é de: \n",imc.toFixed(2)); //reduzir número depois da vírgula
---
+++ nomecompleto.js
@@ -0,0 +1,8 @@
+/*Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima ao usuário*/
+
+var nome= "Anne";
+var sobrenome= "Passos";
+
+nomeCompleto=(nome+" "+sobrenome)
+
+console.log(nomeCompleto)
