3d9bf2869461496bd45670a67b9890e068a74e39
AnnePassos <annecpassos@outlook.com>
2024-09-16 21:27:27 -0300

    Aula 6

---
+++ Exe_2.js
@@ -0,0 +1,15 @@
+/*Vamos escrever numa página os números de 0 até 10.*/
+
+console.log(nomeCompleto)
+/* function contar(){
+    for(var count =0; count <=10 ; count++){
+        document.write(count+"<br />")
+    }
+} */
+
+    function contar() {
+    for (var count =0; count<=10; count++){
+        document.write (count+"<br />")
+    }
+        
+}
---
+++ Exe_3.js
@@ -0,0 +1,8 @@
+/*Vamos escrever numa página os números de 0 até 1000.*/
+
+function contar() {
+    for (var count =0; count<=10; count++){
+        document.write (count+"<br />")
+    }
+        
+}
